b89d8a5e3877034dd58c8b041b51636236c29a71
jmorsan426 <jmorsan426@g.educaand.es>
2022-10-07 13:55:10 +0200

    Añadido FloattingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,14 +6,41 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    const TextStyle miEstilo30 = TextStyle( fontSize: 30, );
+    int cont=0;
     return  Scaffold(
-      backgroundColor: Color.fromARGB(255, 51, 8, 204),
+      backgroundColor: Color.fromARGB(255, 149, 123, 241),
       appBar: AppBar(
-        title: Text('MyApp'),
+        title: const Text('MyApp'),
+        elevation: 10.0,
+        centerTitle: true,
       ),
-      body: const Center(
-          child: Text('Hola Mundo'),
+      body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:  <Widget> [
+              Text('Click Counter',style: miEstilo30,),
+              Text('$cont',style: miEstilo30,),
+            ],
           ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: FloatingActionButton(
+            child:Column(
+              children: const <Widget> [
+                Icon(Icons.add),
+                Icon(Icons.remove),
+              
+            ],
+            ),  
+              onPressed: () {
+              print(cont);
+              
+              cont++;
+            },
+            
+            ),
     );
   }
 
