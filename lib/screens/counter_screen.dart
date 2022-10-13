2f5e0839158387974989f0e1a7a0f2f814525433
jmorsan426 <jmorsan426@g.educaand.es>
2022-10-13 13:49:27 +0200

    Actualizar fuera del widget - Final Seccion 5

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -12,6 +12,21 @@ class _CounterScreenState extends State<CounterScreen> {
 
 int cont=0;
 
+void incrementar(){
+  cont++;
+  setState(() {});
+}
+
+void decrementar(){
+  cont--;
+  setState(() {});
+}
+
+void resetear(){
+  cont=0;
+  setState(() {});
+}
+
   @override
   Widget build(BuildContext context) {
 
@@ -19,7 +34,7 @@ int cont=0;
     
 
     return  Scaffold(
-      backgroundColor: Color.fromARGB(255, 149, 123, 241),
+      backgroundColor: const Color.fromARGB(255, 149, 123, 241),
       appBar: AppBar(
         title: const Text('CounterScreen'),
         elevation: 10.0,
@@ -36,28 +51,51 @@ int cont=0;
           ),
           ),
 
-          //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-              children:  [
-                FloatingActionButton(child: const Icon(Icons.add),
-                onPressed: (){
-                  setState(() {
-                    cont++;
-                  });
-                }),
-
-                FloatingActionButton(child: const Icon(Icons.remove),
-                onPressed: (){
-                  setState(() {
-                    cont--;
-                    });
-                }),
-                
-              
-            ], 
-            ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton:  CustomFloatingActions(
+            incrementarCont: incrementar,
+            decrementarCont: decrementar,
+            resetearCont: resetear,
+          ),
     );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  final Function incrementarCont;
+  final Function decrementarCont;
+  final Function resetearCont;
+
+  const CustomFloatingActions({
+    Key? key, 
+    required this.incrementarCont, 
+    required this.decrementarCont,
+    required this.resetearCont,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      
+         children:  [
+          FloatingActionButton(
+            
+            onPressed:(() => incrementarCont()),
+              child:  const Icon(Icons.add)
+          ),
+          
+           FloatingActionButton(
+             onPressed: (() => resetearCont()),
+              child:  const Icon(Icons.refresh),
+            ),
+
+          FloatingActionButton(
+            onPressed: (() => decrementarCont()),
+            child:  const Icon(Icons.remove)
+            ),
+      ], 
+      );
   }
 }
